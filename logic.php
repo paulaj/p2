cedaed6ed30e3ff643d957fe77ed36e1f0c86f6c
Paula Jacobs <paulajacobso@gmail.com>
2014-07-07 15:45:23 -0400

    Fixed a silly bug regarding length of my wordlist

--- logic.php
+++ logic.php
@@ -26,13 +26,15 @@
         $comment ='You asked for '. $numWords. ' words';
 
         for ($i=0; $i < $numWords; $i++){
-          $wordIndex = rand(0,$numWords-1);
+          $wordIndex = rand(0, sizeof($wordList) -1);
 
           if ($i==0){
             $password .= $wordList[$wordIndex];
+            //echo "At 0 and word is ". $password . "</br>";
           }
           else {
             $password .= "-".$wordList[$wordIndex];
+            //echo "At ".$i. ", Index at ".$wordIndex." and word is ". $password . "</br>";
           }
           
         }
