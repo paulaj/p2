862b578b6b9a51b03ad751be073207b16549c13c
Paula Jacobs <paulajacobso@gmail.com>
2014-07-07 16:51:08 -0400

    Basic functionality is a go. symbol/number/capitalize all work

--- index.php
+++ index.php
@@ -18,7 +18,11 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
       <form method='POST' action='index.php'>
         How many words do you want your password to have? <br>
         <input type='text' name='numWords'><br>
+        <input type="checkbox" name="addNumber" value="true">Add a number<br>
+        <input type="checkbox" name="addSymbol" value="true">Add a symbol <br>
+        <input type="checkbox" name="capitalizeFirst" value="true">Capitalize first letter <br>
         <input type='submit' value='Generate my Password!'><br>
+
       </form>
 
 
--- logic.php
+++ logic.php
@@ -1,39 +1,84 @@
 <?php
       $numWords = 0;
       $password='';
-      $comment = ' ';
+      $comment = '';
+      $addNumber = false;
+      $addSymbol = false;
+      $capitalizeFirst = false;
 
       $wordList= Array(
-        "Dog",
-        "Cat",
-        "Mouse",
-        "Frog",
-        "These",
-        "Words",
+        "dog",
+        "cat",
+        "mouse",
+        "frog",
+        "these",
+        "words",
         "are",
         "not",
         "random");
+
+      $symbolList= Array(
+        "~",
+        "!",
+        "@",
+        "#",
+        "$",
+        "%",
+        "^",
+        "&",
+        "*");
       
       foreach ($_POST as $key => $value) {
         if ($key=="numWords"){
            $numWords= $_POST[$key];
         }
+        else if ($key=="addNumber"){
+          $addNumber=true;
+          $comment .= $key. " is " . $value. "</br>";
+        }
+        else if ($key=="addSymbol"){
+          $addSymbol=true;
+          $comment .= $key ." is ". $value. "</br>";
+        }
+        else if ($key=="capitalizeFirst"){
+          $capitalizeFirst=true;
+          $comment .= $key ." is ". $value. "</br>";
+        }
       }
        
       
-
+       $currentWord= "";
        if ($numWords > 0) {
-        $comment ='You asked for '. $numWords. ' words';
+        $comment .='You asked for '. $numWords. ' words';
 
         for ($i=0; $i < $numWords; $i++){
           $wordIndex = rand(0, sizeof($wordList) -1);
+          $currentWord= $wordList[$wordIndex];
+          
+          
 
           if ($i==0){
-            $password .= $wordList[$wordIndex];
+            if($capitalizeFirst){
+              $currentWord = ucfirst($currentWord);
+            }
+            $password .= $currentWord;
             //echo "At 0 and word is ". $password . "</br>";
           }
+          else if ($i == $numWords -1){
+            $password .= "-". $currentWord ;
+
+            if($addNumber){
+              $password.= rand(0, 9);
+            }
+
+            if($addSymbol){
+               $password.= $symbolList[rand(0, sizeof($symbolList) -1)];
+            }
+             //echo "At 0 and word is ". $password . "</br>"; 
+          }
+
           else {
-            $password .= "-".$wordList[$wordIndex];
+            $password .= "-".$currentWord ;
             //echo "At ".$i. ", Index at ".$wordIndex." and word is ". $password . "</br>";
           }
           
