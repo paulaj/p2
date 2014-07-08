28df8a0fa280dea5b2486c7a2c86e49719aa10a6
Paula Jacobs <paulajacobso@gmail.com>
2014-07-08 10:54:16 -0400

    Added more options. What to separate words with and adding favorite word to the end!

--- index.php
+++ index.php
@@ -21,12 +21,12 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
     }
    
     .main-panel{
-      margin-top:10px;
+      margin-top:30px;
       background-color: white;
       border-color: #5bc0de;
     }
 
-    h1{
+    h1,h3{
       color: #5bc0de;
     }
 
@@ -37,68 +37,94 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 </head>
 <body>
   <div class="row">
-  <div class="col-md-1"></div>
-  <div class="col-md-10">
-  <div class=" main-panel panel panel-default">
-  <div class="panel-body">
-  <div class="row">
-    </br>
-    <h1 class="text-center">XKCD Password Generator</h1></br></div>
-    <div class="row">
-      <div class="col-md-1"></div>
-      <div class="col-md-5">   
-      </br>
-      <h3 class="text-center">Password Parameters</h3>
-      <div class="row">
-        <div class="panel panel-default">
-          <div class="panel-body">
-           <form method='POST' action='index.php'>
-              <input class="form-control"  type='text' name='numWords' placeholder="How many words should your password contain?"><br>
-              <div class="checkbox">
-                <label>
-                  <input type="checkbox" name="addNumber" value="true">Add a number<br>
-                </label>
-              </div>
-              <div class="checkbox">
-                <label>
-                  <input type="checkbox" name="addSymbol" value="true">Add a symbol <br>
-                </label>
-              </div>
-              <div class="checkbox">
-                <label>
-                  <input type="checkbox" name="capitalizeFirst" value="true">Capitalize first letter <br>
-                </label>
-              </div>
-              <button type='submit' class="btn btn-info"> Generate my Password! </button><br> 
-            </form>
-          </div>
-        </div>
-      </div>
-      <div class="row text-center">
-        <h3>Your Shiny New Password:</h3>
-        <div class="panel panel-default">
-          <div class="panel-body">
-            <h1 class="result"> <?php echo $password; ?></h1>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    </br>
-    <div class="col-md-5">
-      <h3 class="text-center"> The Comic That Inspired This </h3>
-      <div class="panel panel-default">
+    <div class="col-md-1"></div>
+    <div class="col-md-10">
+      <div class=" main-panel center-block panel panel-default">
         <div class="panel-body">
-          <img src="images/password_strength.png" alt="Comic #939">
+          <!-- <div class="row"> -->
+            </br>
+            <h1 class="text-center">XKCD Password Generator</h1>
+            <h4 class="text-center">Creates XKCD style passwords</h4></br>
+            <div class="row">
+              <div class="col-md-1"></div>
+              <div class="col-md-5">   
+              </br>
+              <h3 class="text-center">Password Parameters</h3>
+              <div class="row">
+                <div class="panel panel-default">
+                  <div class="panel-body">
+                   <form method='POST' action='index.php'>
+                      <input class="form-control"  type='text' name='numWords' maxlength="2" placeholder="How many words [1-99] should your password contain?"><br>
+                      
+                       Separate words with: 
+                     <label class="radio-inline">
+                        <input type="radio" name="separator"  value="hyphens" checked> Hyphens
+                      </label>
+                      <label class="radio-inline">
+                        <input type="radio" name="separator" value="spaces"> Spaces
+                      </label>
+                      <label class="radio-inline">
+                        <input type="radio" name="separator" value="camelCase"> CamelCase
+                      </label>
+
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox" name="addNumber" value="true">Add a number<br>
+                        </label>
+                      </div>
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox" name="addSymbol" value="true">Add a symbol <br>
+                        </label>
+                      </div>
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox" name="capitalizeFirst" value="true">Capitalize first letter <br>
+                        </label>
+                      </div>
+                                          
+                      <div class="checkbox">
+
+                        <label>
+                          <input type="checkbox" name="includeWord" value="true"> Include your favorite word [45 characters max]<br> 
+                          <input class="form-control"  type='text' name='favoriteWord' maxlength="45" placeholder="Enter your favorite word "><br>
+                        </label>
+                          
+                      </div>
+                      <button type='submit' class="btn btn-info"> Generate my Password! </button><br> 
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <div class="row text-center">
+                <h3>Your Shiny New Password:</h3>
+                <div class="panel panel-default">
+                  <div class="panel-body">
+                    <?php if ($error==''): ?>
+                    <h2 class="result"> <?php echo $password; ?></h2>
+                    <?php else: ?>
+                    <div class="alert alert-danger" role="alert"><?php echo $error; ?></div> 
+                    <?php endif; ?>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            </br>
+            <div class="col-md-5">
+              <h3 class="text-center"> The Comic That Inspired This </h3>
+              <div class="panel panel-default">
+                <div class="panel-body">
+                  <img src="images/password_strength.png" alt="Comic #939">
+                </div>
+              </div>
+            </div>
+            <div class="col-md-1"> </div>
+          <!-- </div> -->
         </div>
       </div>
     </div>
-    <div class="col-md-1"> </div>
+    <div class="col-md-1"></div>
   </div>
-  </div>
-</div>
-</div>
-<div class="col-md-1"></div>
-</div>
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,10 +1,15 @@
 <?php
       $numWords = 0;
       $password='';
-      $comment = '';
+      $error = '';
       $addNumber = false;
       $addSymbol = false;
       $capitalizeFirst = false;
+      $separator="-";
+      $includeWord= false;
+      $favoriteWord='';
+      $camelCase=false;
+
 
       $wordList= Array(
         "dog",
@@ -28,35 +33,93 @@
         "&",
         "*");
       
-      foreach ($_POST as $key => $value) {
-        if ($key=="numWords"){
-           $numWords= $_POST[$key];
+      if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        //Number of Words
+        if(empty($_POST["numWords"])){
+          $error .= "You did not specify number of words. </br>";
         }
-        else if ($key=="addNumber"){
-          $addNumber=true;
-          $comment .= $key. " is " . $value. "</br>";
+        else{
+          if  (preg_match("/^[1-9]*$/",$_POST["numWords"])){
+            $numWords= $_POST["numWords"];
+          }
+          else{
+            $error .= " Please only enter numbers from 1-9 </br>";
+          }
         }
-        else if ($key=="addSymbol"){
-          $addSymbol=true;
-          $comment .= $key ." is ". $value. "</br>";
+
+        if(!empty($_POST["separator"])){
+           if($_POST["separator"] == "hyphens"){
+            $separator = "-";
+           }
+           else if ($_POST["separator"] == "spaces"){
+            $separator = " ";
+           }
+           else{
+            $separator="";
+            $camelCase = true;
+           }
         }
-        else if ($key=="capitalizeFirst"){
-          $capitalizeFirst=true;
-          $comment .= $key ." is ". $value. "</br>";
+        
+
+
+        //Include favorite word
+        if(!empty($_POST["includeWord"])){
+           if(!empty($_POST["favoriteWord"])){
+            $favoriteWord=$_POST["favoriteWord"];
+            if ($camelCase){
+              $favoriteWord = ucfirst($favoriteWord);
+            }
+            $includeWord= true;
+           }
+           else{
+            $error .= " If you want your favorite word included, you have to enter it. </br>";
+           }
+        }
+         //Add a number
+        if(!empty($_POST["addNumber"])){
+           $addNumber=true;
+        }
+        //Add Symbol
+        if(!empty($_POST["addSymbol"])){
+           $addSymbol=true;
+        }
+        //Capitalize First Letter
+        if(!empty($_POST["capitalizeFirst"])){
+           $capitalizeFirst=true;
         }
       }
+
+      // foreach ($_POST as $key => $value) {
+      //   if ($key=="numWords"){
+      //      $numWords= $_POST[$key];
+      //   }
+      //   else if ($key=="addNumber"){
+      //     $addNumber=true;
+      //     $comment .= $key. " is " . $value. "</br>";
+      //   }
+      //   else if ($key=="addSymbol"){
+      //     $addSymbol=true;
+      //     $comment .= $key ." is ". $value. "</br>";
+      //   }
+      //   else if ($key=="capitalizeFirst"){
+      //     $capitalizeFirst=true;
+      //     $comment .= $key ." is ". $value. "</br>";
+      //   }
+      // }
        
       
        $currentWord= "";
        if ($numWords > 0) {
-        $comment .='You asked for '. $numWords. ' words';
+        
 
         for ($i=0; $i < $numWords; $i++){
           $wordIndex = rand(0, sizeof($wordList) -1);
           $currentWord= $wordList[$wordIndex];
+          if ($camelCase){
+              $currentWord = ucfirst($currentWord);
+          }
           
           
-
           if ($i==0){
             if($capitalizeFirst){
               $currentWord = ucfirst($currentWord);
@@ -65,7 +128,10 @@
             //echo "At 0 and word is ". $password . "</br>";
           }
           else if ($i == $numWords -1){
-            $password .= "-". $currentWord ;
+            if($includeWord){
+              $currentWord = $favoriteWord;
+            }
+            $password .= $separator. $currentWord ;
 
             if($addNumber){
               $password.= rand(0, 9);
@@ -74,12 +140,11 @@
             if($addSymbol){
                $password.= $symbolList[rand(0, sizeof($symbolList) -1)];
             }
-             //echo "At 0 and word is ". $password . "</br>"; 
+             
           }
 
           else {
-            $password .= "-".$currentWord ;
-            //echo "At ".$i. ", Index at ".$wordIndex." and word is ". $password . "</br>";
+            $password .= $separator.$currentWord ;
           }
           
         }
